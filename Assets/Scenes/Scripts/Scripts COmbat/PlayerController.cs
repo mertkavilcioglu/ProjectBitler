a64fe806f90043948f7db2d606842fb99ff47ee8
teakdemir <136896802+teakdemir@users.noreply.github.com>
2024-12-19 11:10:56 +0300

    Auto-Shoot left-rigth
    bakılan yöne iki silahtan otomatik ateş etme

--- Assets/Scenes/Scripts/Scripts COmbat/PlayerController.cs
+++ Assets/Scenes/Scripts/Scripts COmbat/PlayerController.cs
@@ -4,38 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Weapon weaponLeft;  
+    public Weapon weaponRight; 
 
-  public float moveSpeed = 5f;
-public Rigidbody2D rb;
-public Weapon weapon;
+    Vector2 moveDirection;
+    Vector2 mousePosition;
 
-Vector2 moveDirection;
-Vector2 mousePosition;
+    private float nextFireTime = 0.5f; 
 
-void Update()
-{
-    
-    float moveX = Input.GetAxisRaw("Horizontal");
-    float moveY = Input.GetAxisRaw("Vertical");
-
-    
-    if (Input.GetMouseButtonDown(0))
+    void Update()
     {
-        weapon.Fire();
+        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        
+        if (Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + 0.5f; 
+
+            
+            weaponRight.Fire();
+
+            
+            weaponLeft.Fire();
+        }
     }
 
-    
-    moveDirection = new Vector2(moveX, moveY).normalized;
-
-    
-    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    void FixedUpdate()
+    {
+        
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
 }
 
-void FixedUpdate()
-{
-    
-    rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-}
-
-
-}
