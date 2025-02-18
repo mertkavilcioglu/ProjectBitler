39fa7e2c31a438e3bcf03078100424d43eed87c5
emirkutluhan <arslan.emirkutluhan@student.atilim.edu.tr>
2025-02-19 02:01:52 +0300

    hatıralar uslu durmaz
    sesleri ayarladım ama diğer scriptlere girip kod yazmam gerekti. kısaca yemedi

---
+++ Assets/audios/AudioManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+	[SerializeField] private AudioSource musicSource;
+	[SerializeField] private AudioSource SFXSource;
+	[Header("---------- Audio Clip ----------")]
+	public AudioClip background;
+	public AudioClip death;
+	public AudioClip sword;
+	public AudioClip cannon;
+	public AudioClip boss;
+ 	public AudioClip menu;
+    public AudioClip shooting;
+    public AudioClip battle;
+    public AudioClip bow;
+
+    private void Start()
+    {
+	    musicSource.clip = background;
+	    musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+	    
+    }
+
+}
