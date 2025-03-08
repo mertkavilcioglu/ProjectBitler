a7660eebf1fe92778dd4a89f9f88952f1335d169
ozgeekoc <ozgekoc2004@gmail.com>
2025-03-08 21:35:28 +0300

    bit artık vol10

--- Assets/Scenes/Menu/PauseManager.cs
+++ Assets/Scenes/Menu/PauseManager.cs
@@ -2,10 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class PauseManager : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    private bool isPaused = false;
+    private void Start()
+    {
+        if (pauseMenu != null)
+            pauseMenu.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
     private void Awake()
     {
         Canvas pauseMenuCanvas = pauseMenu.GetComponent<Canvas>();
@@ -18,24 +37,24 @@ public class PauseManager : MonoBehaviour
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        isPaused = true;
     }
-
     public void Home()
     {
         SceneManager.LoadScene("Menu");
         Time.timeScale = 1;
+        isPaused = false;
     }
-
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        isPaused = false;
     }
-
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
-
+        isPaused = false;
     }
 }
